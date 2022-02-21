f70f4eae00395823b65b45788e0e9bd02a1d588d
Carl Crozier <crozier83@googlemail.com>
2022-02-21 16:29:07 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const questions = [
+  {
+    question:
+      "What year did the Titanic sink in the Atlantic Ocean on 15 April, on its maiden voyage from Southampton?",
+    answers: { a: "1911", b: "1912", c: "1913" },
+    correctAnswer: "b",
+  },
+  {
+    question:
+      "What is the title of the first ever Carry On film made and released in 1958?",
+    answers: {
+      a: "Carry on Sergeant",
+      b: "Carry on Camping",
+      c: "Carry on Nurse",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question:
+      "What is the name of the biggest technology company in South Korea?",
+    answers: { a: "Samsung", b: "Toshiba", c: "Sony" },
+    correctAnswer: "a",
+  },
+];
+
+const question1 = document.getElementById("q1");
+const question2 = document.getElementById("q2");
+const question3 = document.getElementById("q3");
+
+function importQuestion(questions) {}
